fc2f4bf9d628f13e261712f539105160af474a04
Maryia Malakhava <malakhava@yahoo.com>
2022-04-06 23:23:06 -0400

    fixed await

--- server/server.js
+++ server/server.js
@@ -13,14 +13,20 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //  a new Apollo server to pass in schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
+const startServer = async() => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+  await server.start();
 
 // integrate Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -39,8 +45,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
 
